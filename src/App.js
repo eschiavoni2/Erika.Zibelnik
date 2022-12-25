18ea9c6501cab744798b8e177bdec489d55ec3fc
Erika Zibelnik <ezibelnik@valorsystems.com>
2022-12-25 10:59:14 -0800

    correction to routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import React from "react";
 import Home from './pages/index';
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter as HashRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
       <HashRouter>
         <Routes>
-          <Route path="/erika.zibelnik" element={<Home/>}>
+          <Route exact path="/erika.zibelnik" element={<Home/>}>
           </Route>
         </Routes>
       </HashRouter>
