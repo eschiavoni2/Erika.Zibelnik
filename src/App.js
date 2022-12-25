ef089514a964d01b1aa0ba8ee3dc8dc28f1e69a9
Erika Zibelnik <ezibelnik@valorsystems.com>
2022-12-25 10:48:33 -0800

    deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
       <HashRouter>
         <Routes>
-          <Route path="/" element={<Home/>}>
+          <Route path="/erika.zibelnik" element={<Home/>}>
           </Route>
         </Routes>
       </HashRouter>
